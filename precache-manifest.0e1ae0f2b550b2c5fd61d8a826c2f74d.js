359c8cd0b774b1147d21ce4c361a5d007ccc0174
Lazar Furtula <Lazar996F@users.noreply.github.com>
2020-09-20 22:36:43 +0200

    Updates

---
+++ precache-manifest.0e1ae0f2b550b2c5fd61d8a826c2f74d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfb902ee7083b74da073",
+    "url": "/portfolio/static/css/main.93a059d3.chunk.css"
+  },
+  {
+    "revision": "dfb902ee7083b74da073",
+    "url": "/portfolio/static/js/main.dfb902ee.chunk.js"
+  },
+  {
+    "revision": "08879cd432a84a939ce2",
+    "url": "/portfolio/static/css/1.121eb632.chunk.css"
+  },
+  {
+    "revision": "08879cd432a84a939ce2",
+    "url": "/portfolio/static/js/1.08879cd4.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "ffa731306f01ddf9e1c4f738b37b2be4",
+    "url": "/portfolio/static/media/LazarFurtula_CV.ffa73130.pdf"
+  },
+  {
+    "revision": "05892303f3e25df0c4f110d4483d9671",
+    "url": "/portfolio/index.html"
+  }
+];
