4b74a0f24fd8a02893c3ed2a60ea0833faf55b11
Lazar Furtula <Lazar996F@users.noreply.github.com>
2020-09-20 13:14:30 +0200

    CV.pdf failed-no file

--- src/components/About.js
+++ src/components/About.js
@@ -1,44 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 export default class About extends Component {
   render() {
     let resumeData = this.props.resumeData;
     return (
       <section id="about">
-         <div className="row">
+        <div className="row">
+          <div className="three columns">
+            <img className="profile-pic" src="images/profilepic.jpg" alt="" />
+          </div>
 
-            <div className="three columns">
+          <div className="nine columns main-col">
+            <h2>About Me</h2>
+            <p>{resumeData.aboutme}</p>
 
-               <img className="profile-pic"  src="images/profilepic.jpg" alt="" />
-
+            <div className="row">
+              <div className="columns contact-details">
+                <h2>Contact Details</h2>
+                <p className="address">
+                  <span>{resumeData.name}</span>
+                  <br />
+                  <span>{resumeData.address}</span>
+                  <br />
+                  <span>{resumeData.phone}</span>
+                </p>
+                <h2>Here is my CV</h2>
+                <a href="../../public/LazarFurtula_CV.pdf" download>
+                  <svg
+                    width="3em"
+                    height="3em"
+                    viewBox="0 0 16 16"
+                    class="bi bi-file-earmark-arrow-down"
+                    fill="currentColor"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M4 0h5.5v1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h1V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z" />
+                    <path d="M9.5 3V0L14 4.5h-3A1.5 1.5 0 0 1 9.5 3z" />
+                    <path
+                      fill-rule="evenodd"
+                      d="M8 6a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 10.293V6.5A.5.5 0 0 1 8 6z"
+                    />
+                  </svg>
+                </a>
+              </div>
             </div>
-
-            <div className="nine columns main-col">
-
-               <h2>About Me</h2>
-               <p>
-               {
-                 resumeData.aboutme
-               }
-               </p>
-
-               <div className="row">
-
-                  <div className="columns contact-details">
-
-                  <h2>Contact Details</h2>
-                  <p className="address">
-       						<span>{resumeData.name}</span>
-                     <br></br>
-       						   <span>
-                     {resumeData.address}
-                    </span>
-                    <br></br>
-                    <span>{resumeData.phone}</span>
-       					   </p>
-                  </div>
-               </div>
-            </div>
-         </div>
+          </div>
+        </div>
       </section>
     );
   }
